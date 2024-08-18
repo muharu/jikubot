0b78a7d324a401e4d804777c87a3af79b31db575
Muhammad Harun Alrasyid <muharu@proton.me>
2024-08-18 11:24:51 +0700

    refactor: handle discord rate-limit

--- packages/api/src/common/lib/discord.ts
+++ packages/api/src/common/lib/discord.ts
@@ -1,6 +1,7 @@
 import type { FetchLike, WretchOptions } from "wretch";
 import { Routes } from "discord-api-types/v10";
 import wretch from "wretch";
+import { WretchError } from "wretch/resolver";
 
 const DISCORD_API_VERSION = 10;
 const DISCORD_API_BASE_URL = `https://discord.com/api/v${DISCORD_API_VERSION}`;
@@ -9,14 +10,18 @@ const discordApiFetch = wretch(DISCORD_API_BASE_URL);
 
 const discordRatelimitHandlerMiddleware =
   (next: FetchLike) => async (url: string, opts: WretchOptions) => {
-    const response = await next(url, opts);
+    let response = await next(url, opts);
 
     if (response.status === 429) {
-      const resetAfter = Number(
-        response.headers.get("X-RateLimit-Reset-After"),
-      );
-      await new Promise((resolve) => setTimeout(resolve, resetAfter * 1000));
-      return next(url, opts);
+      const retryAfter = Number(response.headers.get("retry-after"));
+      if (!isNaN(retryAfter)) {
+        await new Promise((resolve) => setTimeout(resolve, retryAfter * 1000));
+        response = await next(url, opts);
+      } else {
+        throw new WretchError(
+          "Rate limit exceeded without a Retry-After header.",
+        );
+      }
     }
 
     return response;
