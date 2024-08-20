d40133622389f4fec00789e0aed8453a133edbec
Muhammad Harun Alrasyid <muharu@proton.me>
2024-08-20 07:37:09 +0700

    refactor: update user repository

--- packages/api/src/repositories/user.repository.ts
+++ packages/api/src/repositories/user.repository.ts
@@ -1,59 +1,58 @@
 import { db, eq, tokens, users } from "@giverve/db";
 
-export async function findUserByDiscordId(discordId: number, ctx = db) {
-  return await ctx.query.users.findFirst({
-    where: (users, { eq }) => eq(users.discordId, discordId),
-  });
+type InsertUser = typeof users.$inferInsert;
+type InsertTokens = typeof tokens.$inferInsert;
+
+class UserRepository {
+  private ctx;
+
+  constructor() {
+    this.ctx = db;
+  }
+
+  async findUserByDiscordId(discordId: number, ctx = this.ctx) {
+    return await ctx.query.users.findFirst({
+      where: (users, { eq }) => eq(users.discordId, discordId),
+    });
+  }
+
+  async insertUser(data: InsertUser, ctx = this.ctx) {
+    return await ctx.insert(users).values(data).returning();
+  }
+
+  async updateUserByDiscordId(
+    discordId: number,
+    data: InsertUser,
+    ctx = this.ctx,
+  ) {
+    return await ctx
+      .update(users)
+      .set(data)
+      .where(eq(users.discordId, discordId))
+      .returning();
+  }
+
+  async findUserTokensByDiscordId(userId: number, ctx = this.ctx) {
+    return await ctx.query.tokens.findFirst({
+      where: (tokens, { eq }) => eq(tokens.discordId, userId),
+    });
+  }
+
+  async insertUserTokens(data: InsertTokens, ctx = this.ctx) {
+    return await ctx.insert(tokens).values(data).returning();
+  }
+
+  async updateUserTokensByDiscordId(
+    discordId: number,
+    data: InsertTokens,
+    ctx = this.ctx,
+  ) {
+    return await ctx
+      .update(tokens)
+      .set(data)
+      .where(eq(tokens.discordId, discordId))
+      .returning();
+  }
 }
 
-export async function insertUser(data: typeof users.$inferInsert, ctx = db) {
-  return await ctx.insert(users).values(data).returning();
-}
-
-export async function updateUserByDiscordId(
-  discordId: number,
-  data: typeof users.$inferInsert,
-  ctx = db,
-) {
-  return await ctx
-    .update(users)
-    .set(data)
-    .where(eq(users.discordId, discordId))
-    .returning();
-}
-
-export async function findUserTokensByDiscordId(userId: number, ctx = db) {
-  return await ctx.query.tokens.findFirst({
-    where: (tokens, { eq }) => eq(tokens.discordId, userId),
-  });
-}
-
-export async function insertUserTokens(
-  data: typeof tokens.$inferInsert,
-  ctx = db,
-) {
-  return await ctx.insert(tokens).values(data).returning();
-}
-
-export async function updateUserTokensByDiscordId(
-  discordId: number,
-  data: typeof tokens.$inferInsert,
-  ctx = db,
-) {
-  return await ctx
-    .update(tokens)
-    .set(data)
-    .where(eq(tokens.discordId, discordId))
-    .returning();
-}
-
-const userRepository = {
-  findUserByDiscordId,
-  insertUser,
-  updateUserByDiscordId,
-  findUserTokensByDiscordId,
-  insertUserTokens,
-  updateUserTokensByDiscordId,
-};
-
-export default userRepository;
+export default new UserRepository();
