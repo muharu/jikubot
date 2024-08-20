f6f8ed118b9f50644714b56bac2213c3971cb2e7
Muhammad Harun Alrasyid <muharu@proton.me>
2024-08-20 10:00:18 +0700

    refactor: remove dtos and add comments to auth router

--- packages/api/src/routers/auth.router.ts
+++ packages/api/src/routers/auth.router.ts
@@ -1,24 +1,21 @@
 import discord from "../common/discord";
-import authUserDTOMapper from "../dtos/auth.dto";
 import authSchema from "../schemas/auth.schema";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const authRouter = createTRPCRouter({
   login: publicProcedure
     .output(authSchema.loginResponse)
-    .mutation(({ ctx }) => {
-      const { res, common } = ctx;
-
+    .mutation(({ ctx: { res, common } }) => {
+      // Generate a random state and Discord authorization URL
       const authState = common.utils.generateRandomString(43);
       const url = discord.generateDiscordAuthorizationUrl(authState);
 
+      // Set OAuth state cookie with 1-hour expiration
       common.cookies.setCookie(
         res,
         common.constants.COOKIE_OAUTH_STATE_NAME,
         authState,
-        {
-          maxAge: 60 * 60, // 1 hour
-        },
+        { maxAge: 60 * 60 }, // 1 hour
       );
 
       return { url };
@@ -27,24 +24,36 @@ export const authRouter = createTRPCRouter({
   authorize: publicProcedure
     .input(authSchema.authorizeRequest)
     .output(authSchema.authorizeResponse)
-    .mutation(async ({ ctx, input }) => {
-      const { code } = input;
-      const { res, common, services } = ctx;
-
+    .mutation(async ({ ctx: { res, common, services }, input: { code } }) => {
+      // Exchange authorization code for tokens
       const { access_token, refresh_token, expires_in } =
         await services.auth.exchangeAuthorizationCodeForToken(code);
+
+      // Get user info from Discord
       const userInfoFromDiscord =
         await services.auth.exchangeAccessTokenForUserInfo(access_token);
 
-      const user = authUserDTOMapper.convertToAuthUserDTO(userInfoFromDiscord);
+      // Convert Discord user info to our user object
+      const user = {
+        id: parseInt(userInfoFromDiscord.id),
+        username: String(userInfoFromDiscord.username),
+        email: String(userInfoFromDiscord.email),
+        avatar: String(userInfoFromDiscord.avatar),
+        globalName: String(userInfoFromDiscord.global_name),
+      };
+
+      // Calculate JWT expiration time
       const jwtExpirationTime = new Date(Date.now() + expires_in * 1000);
 
+      // Sign the JWT
       const jwt = await common.utils.signJWT(user, jwtExpirationTime);
 
+      // Encrypt tokens and JWT
       const encryptedAccessToken = common.utils.encryptString(access_token);
       const encryptedRefreshToken = common.utils.encryptString(refresh_token);
       const encryptedJWT = common.utils.encryptString(jwt);
 
+      // Save or update user and tokens
       await services.auth.saveOrUpdateUser(user.id, {
         discordId: user.id,
         username: user.username,
@@ -59,29 +68,26 @@ export const authRouter = createTRPCRouter({
         refreshToken: refresh_token,
       });
 
+      // Set cookies for access token, refresh token, and JWT
       common.cookies.setCookie(
         res,
         common.constants.COOKIE_ACCESS_TOKEN_NAME,
         encryptedAccessToken,
-        {
-          maxAge: expires_in,
-        },
+        { maxAge: expires_in },
       );
+
       common.cookies.setCookie(
         res,
         common.constants.COOKIE_REFRESH_TOKEN_NAME,
         encryptedRefreshToken,
-        {
-          maxAge: expires_in,
-        },
+        { maxAge: expires_in },
       );
+
       common.cookies.setCookie(
         res,
         common.constants.COOKIE_JWT_NAME,
         encryptedJWT,
-        {
-          maxAge: expires_in,
-        },
+        { maxAge: expires_in },
       );
 
       return user;
