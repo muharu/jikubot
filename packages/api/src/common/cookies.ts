6e105924d7d0c792cd758509fff3a20b94312bc8
Muhammad Harun Alrasyid <muharu@proton.me>
2024-08-20 19:30:04 +0700

    refactor: simplify common cookies

--- packages/api/src/common/cookies.ts
+++ packages/api/src/common/cookies.ts
@@ -1,32 +1,15 @@
-import type { CookieParseOptions, CookieSerializeOptions } from "cookie";
+import type { CookieSerializeOptions } from "cookie";
 import type { IncomingMessage, ServerResponse } from "http";
 import { parse as parseCookie, serialize as serializeCookie } from "cookie";
 
 export class Cookies {
-  private serialize(
-    name: string,
-    value: string,
-    options?: CookieSerializeOptions,
-  ): string {
-    return serializeCookie(name, value, options);
-  }
-
-  private parse(
-    cookieHeader?: string,
-    options?: CookieParseOptions,
-  ): Record<string, string> {
-    return cookieHeader != null && cookieHeader !== ""
-      ? parseCookie(cookieHeader, options)
-      : {};
-  }
-
   public setCookie(
     res: ServerResponse,
     name: string,
     value: string,
     options: CookieSerializeOptions = {},
   ) {
-    const serializedCookie = this.serialize(name, value, {
+    const serializedCookie = serializeCookie(name, value, {
       httpOnly: options.httpOnly ?? process.env.NODE_ENV === "production",
       secure: options.secure ?? process.env.NODE_ENV === "production",
       path: options.path ?? "/",
@@ -49,7 +32,10 @@ export class Cookies {
 
   public getCookie(req: IncomingMessage, name: string) {
     const cookieHeader = req.headers.cookie;
-    const cookies = this.parse(cookieHeader);
+    const cookies =
+      cookieHeader != null && cookieHeader !== ""
+        ? parseCookie(cookieHeader)
+        : {};
     return cookies[name];
   }
 
@@ -58,7 +44,7 @@ export class Cookies {
     name: string,
     options?: CookieSerializeOptions,
   ) {
-    const serializedCookie = this.serialize(name, "", {
+    const serializedCookie = serializeCookie(name, "", {
       expires: new Date(0),
       path: options?.path ?? "/",
       sameSite: options?.sameSite ?? "lax",
