95eb0f04f52a79e6bd9cc961f011a22ff7a2c95e
Muhammad Harun Alrasyid <muharu@proton.me>
2024-08-24 13:00:08 +0700

    feat: add lfg to sidebar menu

--- apps/web/src/layouts/dashboard-layout.tsx
+++ apps/web/src/layouts/dashboard-layout.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { AiTwotoneDashboard } from "react-icons/ai";
+import { HiUserGroup } from "react-icons/hi2";
 
 import { cn } from "@giverve/ui";
 import { buttonVariants } from "@giverve/ui/button";
@@ -35,18 +36,39 @@ function Sidebar() {
   const router = useRouter();
   const guildId = String(router.query.guildId);
 
+  const isActive = (href: string) => router.pathname === href;
+
+  console.log(router.pathname);
+
   return (
     <ScrollArea className="hidden h-[100dvh] w-[350px] rounded-md border-r bg-white p-4 lg:flex">
-      <nav className="flex flex-col gap-y-2">
+      <nav className="flex flex-col gap-y-3">
         <Link
           href={`/dashboard/${guildId}`}
           className={cn(
-            buttonVariants({ variant: "default" }),
+            buttonVariants({
+              variant: isActive(`/dashboard/[guildId]`) ? "default" : "neutral",
+            }),
             "w-[95%] justify-start",
           )}
         >
           <AiTwotoneDashboard className="mr-2 size-7" />
           Dashboard
+        </Link>
+
+        <Link
+          href={`/dashboard/${guildId}/lfg`}
+          className={cn(
+            buttonVariants({
+              variant: isActive(`/dashboard/[guildId]/lfg`)
+                ? "default"
+                : "neutral",
+            }),
+            "w-[95%] justify-start",
+          )}
+        >
+          <HiUserGroup className="mr-2 size-7" />
+          Looking for Group
         </Link>
       </nav>
     </ScrollArea>
