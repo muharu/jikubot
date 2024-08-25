59d86324650badb688b8247f4e8b4ef74e2ef355
Muhammad Harun Alrasyid <muharu@proton.me>
2024-08-26 05:43:44 +0700

    refactor: event creation components

--- apps/web/src/pages/dashboard/[guildId]/events/index.tsx
+++ apps/web/src/pages/dashboard/[guildId]/events/index.tsx
@@ -1,24 +1,5 @@
-import { RiAddLine } from "react-icons/ri";
-
-import { Button } from "@giverve/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@giverve/ui/dialog";
-import {
-  Drawer,
-  DrawerContent,
-  DrawerDescription,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@giverve/ui/drawer";
-
-import CreateEventForm from "~/features/dashboard/[guildId]/events/components/create-event-form";
+import DrawerCreateEvent from "~/features/dashboard/[guildId]/events/components/drawer-create-event";
+import ModalCreateEvent from "~/features/dashboard/[guildId]/events/components/modal-create-event";
 import useDashboardCheck from "~/hooks/use-dashboard-check";
 import DashboardLayout from "~/layouts/dashboard-layout";
 import GlobalLoading from "~/layouts/global-loading";
@@ -38,59 +19,10 @@ export default function Dashboard() {
             <h1 className="text-2xl">Events</h1>
             <span>Create new event for your community in discord server</span>
           </div>
-          <ModalCreateButton />
+          <ModalCreateEvent />
         </div>
-        <DrawerCreateButton />
+        <DrawerCreateEvent />
       </main>
     </DashboardLayout>
-  );
-}
-
-function ModalCreateButton() {
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button>
-          <RiAddLine className="mr-1.5 size-5" />
-          New
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[500px]">
-        <DialogHeader>
-          <DialogTitle>Create Event</DialogTitle>
-          <DialogDescription>
-            Create a new event for your community in discord server
-          </DialogDescription>
-        </DialogHeader>
-        <CreateEventForm type="dialog" />
-      </DialogContent>
-    </Dialog>
-  );
-}
-
-function DrawerCreateButton() {
-  return (
-    <Drawer>
-      <DrawerTrigger asChild>
-        <Button
-          size="icon"
-          variant="noShadow"
-          className="absolute bottom-10 right-5 h-14 w-14 rounded-full lg:hidden"
-        >
-          <RiAddLine className="size-8" />
-        </Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <div className="px-4">
-          <DrawerHeader>
-            <DrawerTitle>Create Event</DrawerTitle>
-            <DrawerDescription>
-              Create a new event for your community
-            </DrawerDescription>
-          </DrawerHeader>
-          <CreateEventForm type="drawer" />
-        </div>
-      </DrawerContent>
-    </Drawer>
   );
 }
