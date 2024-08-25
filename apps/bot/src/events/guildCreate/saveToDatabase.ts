75784022ba9da7714ff4ba62f20be46363304b90
Muhammad Harun Alrasyid <muharu@proton.me>
2024-08-25 07:28:54 +0700

    refactor: add retry logic for adding and leaving guilds

--- apps/bot/src/events/guildCreate/saveToDatabase.ts
+++ apps/bot/src/events/guildCreate/saveToDatabase.ts
@@ -1,15 +1,18 @@
 import type { Guild } from "discord.js";
 
 import { trpc } from "../../trpc";
+import { retry } from "../../utils";
 
 export default async (guild: Guild) => {
   try {
-    await trpc.bot.guilds.add.mutate({
-      guildId: Number(guild.id),
-      ownerId: Number(guild.ownerId),
-      name: guild.name,
-      icon: guild.icon,
-    });
+    await retry(() =>
+      trpc.bot.guilds.add.mutate({
+        guildId: Number(guild.id),
+        ownerId: Number(guild.ownerId),
+        name: guild.name,
+        icon: guild.icon,
+      }),
+    );
     console.log(`Added guild ${guild.name} to the database`);
   } catch (error) {
     console.error(error);
