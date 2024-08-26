e460a929ea562400b252d591110faa43ce242b2f
Muhammad Harun Alrasyid <muharu@proton.me>
2024-08-26 18:49:25 +0700

    feat: Refactor router structure for bot and dashboard

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -3,15 +3,19 @@ import { dashboardEventRouter } from "./routers/event.router";
 import { botUserRouter, dashboardUserRouter } from "./routers/user.router";
 import { createTRPCRouter } from "./trpc";
 
+const botRouter = createTRPCRouter({
+  user: botUserRouter,
+});
+
+const dashboardRouter = createTRPCRouter({
+  auth: dashboardAuthRouter,
+  event: dashboardEventRouter,
+  user: dashboardUserRouter,
+});
+
 export const appRouter = createTRPCRouter({
-  bot: {
-    user: botUserRouter,
-  },
-  dashboard: {
-    auth: dashboardAuthRouter,
-    event: dashboardEventRouter,
-    user: dashboardUserRouter,
-  },
+  bot: botRouter,
+  dashboard: dashboardRouter,
 });
 
 export type AppRouter = typeof appRouter;
