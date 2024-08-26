0c19495aca67b9e131b6eda21eb71b6c1015e436
Muhammad Harun Alrasyid <muharu@proton.me>
2024-08-26 17:30:44 +0700

    refactor: change event validators directly from packages

--- packages/validators/src/event.ts
+++ packages/validators/src/event.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
-export const eventCreateRequest = z.object({
+export const createEventRequestValidator = z.object({
   guildId: z.string(),
   title: z.string().min(3).max(50),
   description: z.string().max(150),
 });
 
-export const eventCreateResponse = z.object({
+export const createEventResponseValidator = z.object({
   eventId: z.string(),
 });
