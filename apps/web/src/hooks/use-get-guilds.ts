87822cdc19a4d954000c9d1e05a8fabcc8af20bf
Muhammad Harun Alrasyid <muharu@proton.me>
2024-09-01 00:53:30 +0700

    refactor: optimize useGetGuilds to refetch on guild invite

--- apps/web/src/hooks/use-get-guilds.ts
+++ apps/web/src/hooks/use-get-guilds.ts
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/router";
 
 import { useGuildsListStore } from "~/state/guilds-list-store";
@@ -6,26 +7,39 @@ import { trpc } from "~/utils/trpc";
 
 export function useGetGuilds() {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
-  const guilds = useGuildsListStore((state) => state.guilds);
-  const setGuilds = useGuildsListStore((state) => state.setGuilds);
+  const code = searchParams.get("code");
+  const guildId = searchParams.get("guild_id");
+  const permissions = searchParams.get("permissions");
+
+  const gotQueryParams = code ?? guildId ?? permissions;
+
+  const { guilds, setGuilds } = useGuildsListStore((state) => ({
+    guilds: state.guilds,
+    setGuilds: state.setGuilds,
+  }));
 
   const query = trpc.dashboard.guilds.getAll.useQuery(undefined, {
-    retry(failureCount, error) {
+    retry: (failureCount, error) => {
       if (error.data?.code === "UNAUTHORIZED") {
         void router.replace("/login");
         return false;
       }
       return failureCount < 3;
     },
-    initialData: guilds.length ? guilds : undefined,
+    initialData: guilds.length && !gotQueryParams ? guilds : undefined,
   });
 
   useEffect(() => {
+    if (gotQueryParams) {
+      void router.replace(router.pathname, undefined, { shallow: true });
+    }
+
     if (query.isSuccess) {
       setGuilds(query.data);
     }
-  }, [query.data, query.isSuccess, setGuilds]);
+  }, [gotQueryParams, query.isSuccess, query.data, router, setGuilds]);
 
   return query;
 }
